e4a9d2fcda1a8c18cc6a2311405196f23b3f38d8
Sefika Efeoglu <sefeoglu@MB-K1GV17JYMC.local>
2026-06-05 09:53:28 +0200

    add prototype

---
+++ src/memory/declarative_memory.py
@@ -0,0 +1,17 @@
+
+class DeclarativeMemory(object):
+    def __init__(self):
+        self.memory = {}
+
+    def store(self, key, value):
+        self.memory[key] = value
+
+    def retrieve(self, key):
+        return self.memory.get(key, None)
+
+    def delete(self, key):
+        if key in self.memory:
+            del self.memory[key]
+
+    def clear(self):
+        self.memory.clear()
